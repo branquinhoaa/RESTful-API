950d39f821589dec9a1c7112639cd9c8760f5b57
branquinhoaa <amanda.alves15@gmail.com>
2018-05-10 08:41:30 -0300

    finish api and include tests for it

--- restful_api.py
+++ restful_api.py
@@ -1,9 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, url_for, abort, g
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, Restaurant
+from models import Base, Restaurant, User
 from findARestaurant import findARestaurant as find
 
+from flask.ext.httpauth import HTTPBasicAuth
+import json
+import httplib2
+from flask import make_response
+
+auth = HTTPBasicAuth()
 
 engine = create_engine('sqlite:///restaurants.db')
 Base.metadata.bind = engine
@@ -17,29 +23,31 @@ app = Flask(__name__)
 # Here I can create my api routes with the specific action for each one
 
 @app.route("/restaurants", methods=['GET', 'POST'])
+@auth.login_required
 def allRestaurants():
 	if request.method == 'GET':
 		return getAllRestaurants()
 	if request.method == 'POST':
-		location = request.args.get("location")
-		mealtype = request.args.get("mealtype")
+		location = request.json.get("location")
+		mealtype = request.json.get("mealtype")
+		print(request.args)
 		return createARestaurant(location, mealtype)
 
 @app.route("/restaurants/<int:id>", methods=['GET', 'PUT', 'DELETE'])
+@auth.login_required
 def restaurant(id):
 	if request.method == 'GET':
 		return getARestaurant(id)
 	if request.method == 'PUT':
-		name = request.args.get('name', '')
-		address = request.args.get('address', '')
-		image = request.args.get('image', '')
+		name = request.json.get('name')
+		address = request.json.get('address')
+		image = request.json.get('image')
 		return updateRestaurant(id, name, address, image)
 	if request.method == 'DELETE':
 		return deleteRestaurant(id)
 
 
 # Generate methods to call - first endpoint
-
 def getAllRestaurants():
 	restaurants = session.query(Restaurant).all()
 	return jsonify(Restaurants=[restaurant.serialize for restaurant in restaurants])
@@ -87,6 +95,34 @@ def deleteRestaurant(id):
 	session.delete(restaurant)
 	session.commit
 	return "restaurant deleted"
+
+# route to create my user
+
+@app.route('/users', methods=['POST'])
+def create_user():
+	if request.method == 'POST':
+		name = request.json.get('username')
+		password = request.json.get('password')
+		if name is None or password is None:
+			abort(400)
+		if session.query(User).filter_by(username=name).first():
+			return("User is already created")
+		user = User(username=name)
+		user.hash_password(password)
+		session.add(user)
+		session.commit()
+		return jsonify(User=user.username)
+
+# authentication for my user
+
+@auth.verify_password
+def verify_password(username, password):
+	user = session.query(User).filter_by(username= username).first()
+	if not user or not user.verify_password(password):
+		return False
+	g.user = user
+	return True
+
 
 
 if __name__ == '__main__':
